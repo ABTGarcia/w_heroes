c9000def8af586f7dbcc4abb2fabb315b9971a36
Alberto Garcia <agarcia4@example.com>
2025-06-29 08:28:03 +0200

    HeroDetailView improved

--- WallaMarvel/Packages/Presentation/Sources/HeroDetail/HeroDetailViewData.swift
+++ WallaMarvel/Packages/Presentation/Sources/HeroDetail/HeroDetailViewData.swift
@@ -3,14 +3,16 @@ import Domain
 
 public struct HeroDetailViewData: Equatable, Sendable {
     let name: String
+    let realName: String
     let image: String
     let deck: String
     let creators: [String]
     let enemies: [String]
     let friends: [String]
 
-    public init(name: String, image: String, deck: String, creators: [String], enemies: [String], friends: [String]) {
+    public init(name: String, realName: String, image: String, deck: String, creators: [String], enemies: [String], friends: [String]) {
         self.name = name
+        self.realName = realName
         self.image = image
         self.deck = deck
         self.creators = creators
