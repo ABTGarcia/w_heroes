0c310b2192ef5d72cdb3596ad8c25d07e07d8a68
Alberto Garcia <agarcia4@example.com>
2025-06-30 10:41:34 +0200

    Set entities to conform Model

--- WallaMarvel/Packages/Data/Sources/Entities/HeroEntity.swift
+++ WallaMarvel/Packages/Data/Sources/Entities/HeroEntity.swift
@@ -1,14 +1,49 @@
 import Domain
+import SwiftData
 
-struct HeroEntity: Codable, Equatable {
-    let id: Int
-    let name: String
-    let realName: String?
-    let deck: String?
-    let image: ImageEntity
-    let apiDetailUrl: String
+@Model
+class HeroEntity: Codable, Equatable {
+    var id: Int
+    var name: String
+    var realName: String?
+    var deck: String?
+    var image: ImageEntity
+    var apiDetailUrl: String
+
+    init(id: Int, name: String, realName: String? = nil, deck: String? = nil, image: ImageEntity, apiDetailUrl: String) {
+        self.id = id
+        self.name = name
+        self.realName = realName
+        self.deck = deck
+        self.image = image
+        self.apiDetailUrl = apiDetailUrl
+    }
+
+    enum CodingKeys: CodingKey {
+        case id, name, realName, deck, image, apiDetailUrl
+    }
 
     public func toDomain() -> Hero {
         Hero(id: String(id), image: image.smallUrl, thumbnail: image.iconUrl, name: name, realName: realName, description: deck, apiDetailUrl: apiDetailUrl)
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        realName = try container.decodeIfPresent(String.self, forKey: .realName)
+        deck = try container.decodeIfPresent(String.self, forKey: .deck)
+        image = try container.decode(ImageEntity.self, forKey: .image)
+        apiDetailUrl = try container.decode(String.self, forKey: .apiDetailUrl)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(realName, forKey: .realName)
+        try container.encode(deck, forKey: .deck)
+        try container.encode(image, forKey: .image)
+        try container.encode(apiDetailUrl, forKey: .apiDetailUrl)
     }
 }
--- WallaMarvel/Packages/Data/Sources/Entities/ImageEntity.swift
+++ WallaMarvel/Packages/Data/Sources/Entities/ImageEntity.swift
@@ -1,5 +1,34 @@
-struct ImageEntity: Codable, Equatable {
-    let iconUrl: String
-    let smallUrl: String
-    let screenUrl: String
+import Foundation
+import SwiftData
+
+@Model
+class ImageEntity: Codable, Equatable {
+    @Attribute(.unique) var id: UUID = UUID()
+    var iconUrl: String
+    var smallUrl: String
+    var screenUrl: String
+
+    init(iconUrl: String, smallUrl: String, screenUrl: String) {
+        self.iconUrl = iconUrl
+        self.smallUrl = smallUrl
+        self.screenUrl = screenUrl
+    }
+
+    enum CodingKeys: CodingKey {
+        case iconUrl, smallUrl, screenUrl
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        iconUrl = try container.decode(String.self, forKey: .iconUrl)
+        smallUrl = try container.decode(String.self, forKey: .smallUrl)
+        screenUrl = try container.decode(String.self, forKey: .screenUrl)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(iconUrl, forKey: .iconUrl)
+        try container.encode(smallUrl, forKey: .smallUrl)
+        try container.encode(screenUrl, forKey: .screenUrl)
+    }
 }
